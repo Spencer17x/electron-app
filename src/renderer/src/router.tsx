280def25b87af3ef3d461255190c8f7f6c1e969a
Spencer17x <1253478653@qq.com>
2023-03-21 14:43:04 +0800

    fix: hash router

--- src/renderer/src/router.tsx
+++ src/renderer/src/router.tsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import PageA from './components/PageA'
 import PageB from './components/PageB'
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     element: <PageA />,
     path: '/page-a'
